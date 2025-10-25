b44b3b5593a683d491b89214c15db8e9f8982cbc
Flaviohenrique2009 <fh001910372@gmail.com>
2025-10-24 21:17:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// alert(
+//     'bem vindos ao meu portifolio'
+// )
+
+function mostrarSobre(){
+    const p=document.getElementById('Sobre-mim')
+    if (p.style.display =='none')
+    p.style.display='block'
+    else
+        p.style.display='none'
+}
